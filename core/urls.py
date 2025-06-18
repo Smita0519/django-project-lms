848c1f40d6b943e14a24b2c0983abcdf5f283ae9
smita <smitamhz13@gmail.com>
2025-06-18 14:55:08 +0545

    Django project LMS: fontawesome, google fonts, static css, teacher EDIT, UPDATE and DELETE

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from core.views import HomePageView, TeacherCreateView, TeacherListView
+from core.views import (HomePageView, TeacherCreateView, TeacherListView, TeacherDetailView, TeacherDeleteView, TeacherUpdateView)
 
 urlpatterns =  [
     path('', HomePageView.as_view, name = 'home'),    # '' no name means takes base url
     path('teacher/list/', TeacherListView.as_view(), name='teacher.index'),
-    path('teacher/create/', TeacherCreateView.as_view(), name='teacher.create')
+    path('teacher/create/', TeacherCreateView.as_view(), name='teacher.create'),
+    path('teacher/<int:pk>/detail/', TeacherDetailView.as_view(), name='teacher.detail'),
+    path('teacher/<int:pk>/edit/', TeacherUpdateView.as_view(), name='teacher.edit'),
+    path('teacher/<int:pk>/delete/', TeacherUpdateView.as_view(), name='teacher.delete'),
+
 ]
