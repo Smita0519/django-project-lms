83b9f4655e600dfa55bf43a70763dbb4cfeceb7d
smita <smitamhz13@gmail.com>
2025-06-19 14:50:53 +0545

    Django Project LMS:login, logout, session management

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from core.models import Student, Teacher, Assignment, Materials
+from core.models import Student, Teacher, Assignment, Material
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Assignment)
-admin.site.register(Materials)
+admin.site.register(Material)
